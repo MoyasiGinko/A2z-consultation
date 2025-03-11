b919e7e5289439d8679cc1e606d819e00d1b3672
moyasiginko <mrsiyam61@gmail.com>
2025-03-12 04:12:08 +0600

    change size and set data

--- components/Hero/HeroDesktop.tsx
+++ components/Hero/HeroDesktop.tsx
@@ -129,11 +129,11 @@ const HeroDesktop: React.FC = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
           ),
-          text: "Full Documentation",
+          text: "Affordable Services",
           highlighted: true,
         },
         {
@@ -149,11 +149,11 @@ const HeroDesktop: React.FC = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
               />
             </svg>
           ),
-          text: "Fast Turnaround",
+          text: "Peace of Mind",
           highlighted: false,
         },
         {
@@ -169,11 +169,11 @@ const HeroDesktop: React.FC = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z"
+                d="M13 10V3L4 14h7v7l9-11h-7z"
               />
             </svg>
           ),
-          text: "Expert Analysis",
+          text: "Customer Focus",
           highlighted: true,
         },
       ],
@@ -196,11 +196,11 @@ const HeroDesktop: React.FC = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
+                d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
           ),
-          text: "Secure Process",
+          text: "Affordable Services",
           highlighted: true,
         },
         {
@@ -216,11 +216,11 @@ const HeroDesktop: React.FC = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
+                d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
               />
             </svg>
           ),
-          text: "Family Support",
+          text: "Peace of Mind",
           highlighted: false,
         },
         {
@@ -236,11 +236,11 @@ const HeroDesktop: React.FC = () => {
                 strokeLinecap="round"
                 strokeLinejoin="round"
                 strokeWidth={2}
-                d="M3 21v-4m0 0V5a2 2 0 012-2h6.5L15 7h5a2 2 0 012 2v8a2 2 0 01-2 2h-15a2 2 0 01-2-2zm12-10v.01"
+                d="M13 10V3L4 14h7v7l9-11h-7z"
               />
             </svg>
           ),
-          text: "Document Management",
+          text: "Customer Focus",
           highlighted: true,
         },
       ],
@@ -819,7 +819,7 @@ const HeroDesktop: React.FC = () => {
                   style={{ zIndex: 0 }}
                 >
                   <motion.div
-                    className={`h-full w-full ${
+                    className={`h-17 w-full ${
                       feature.highlighted
                         ? "bg-gradient-to-r from-blue-300 to-blue-400"
                         : "bg-gradient-to-r from-blue-100 to-blue-200"
@@ -838,7 +838,7 @@ const HeroDesktop: React.FC = () => {
                 <div className="relative z-10 mr-2 flex-shrink-0 text-lg sm:text-xl">
                   {feature.icon}
                 </div>
-                <div className="relative z-10 text-xs sm:text-sm">
+                <div className="relative z-10 text-xs font-bold sm:text-sm">
                   {feature.text}
                 </div>
               </motion.div>
