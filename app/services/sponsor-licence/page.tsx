bd278daa9478a782825143371eb616e5c54ef0e0
moyasiginko <mrsiyam61@gmail.com>
2025-03-12 19:15:16 +0600

    Update page.tsx

--- app/services/sponsor-licence/page.tsx
+++ app/services/sponsor-licence/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Metadata } from "next";
 import BoosterCard from "@/components/Boosters";
 import SponsorBanner from "@/components/Services/SponsorPage";
+import Brands from "@/components/Brands";
+import StatsSection from "@/components/Stats";
 
 export const metadata: Metadata = {
   title: "Sponsor Licence Page - A2Z",
@@ -13,6 +15,8 @@ const SponsorPage = () => {
   return (
     <div className="pb-20 pt-40">
       <SponsorBanner />
+      <Brands />
+      <StatsSection />
     </div>
   );
 };
